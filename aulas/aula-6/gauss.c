aeb9166bebbf27bdcd74c00248b9a5b3873965e3
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2022-09-22 09:41:53 -0300

    Modified aula-6 algorithms

--- aulas/aula-6/gauss.c
+++ aulas/aula-6/gauss.c
@@ -1,47 +1,70 @@
 #include <stdio.h>
 
-#define l 4
-#define c 5
+#define numRows 4
+#define numCols 5
 
-void gauss(double E[l][c]) {
-  for (int j = 0; j < c - 2; j++) {
-    for (int i = j; i < l; i++) {
-      if (E[i][j] != 0) {
-        if (i != j) {
-          // Troca as linhas i e j
-          for (int k = 0; k < c; k++) {
-            double temp[c] = {};
-            temp[k] = E[i][k];
-            E[i][k] = E[j][k];
-            E[j][k] = *temp;
-          }
-        }
-        // Zerar todos os elementos da coluna j abaixo do elemento na posição j, j
-        for (int k = j; k < c; k++) {
-          double m = -E[k][j] / E[j][j];
-          for (int p = j; p < c; p++)
-            E[k][p] = m * E[j][p] + E[k][p];
-        }
-        break;
-      }
+void print_matrix(double m[numRows][numCols]) {
+  for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < numCols; j++) {
+      printf("%.8lf\t", m[i][j]);
     }
+    printf("\n");
   }
 }
 
-void printMatrix(double m[l][c]) {
-  for (int i = 0; i < l; i++) {
-    for (int j = 0; j < c; j++)
-      printf("%.2lf", m[i][j]);
+// apenas funciona para sistemas n x n
+void gauss(double E[numRows][numCols]) {
+  for (int j = 0; j < numCols - 1; j++) { 
+    // percorrer todas as linhas da matriz
+    for (int i = j; i < numRows; i++) { 
+      // para cada linha, percorrer todas as colunas a partir da diagonal
+
+      if (E[i][j] == 0)
+        continue;
+      // não faz nada se o elemento já for zero
+
+      if (i != j) {
+        // Se o elemento não pertence à diagonal
+        for (int k = 0; k < numCols; k++) {
+          // Trocar linhas
+          double temp = E[i][k];
+          E[i][k] = E[j][k];
+          E[j][k] = temp;
+        }
+      }
+
+      for (int k = 0; k < numRows; k++) {
+        if (k == j)
+          continue;
+
+        double m = -E[k][j] / E[j][j];
+        for (int p = j; p < numCols; p++)
+          E[k][p] = m * E[j][p] + E[k][p];
+        // Operação elementar com todos os elementos da linha
+      } // zerar todos os elementos da coluna j que não estão na diagonal
+
+      printf("\n");
+      print_matrix(E);
+      break;
+    }
+  }
+
+  // soluções do sistema
+  printf("\n");
+  for (int i = 0; i < numRows; i++) {
+    double xi = E[i][numCols - 1] / E[i][i];
+    printf("x_%d = %.16lf\t", i + 1, xi);
   }
 }
 
 int main() {
-  double E[l][c] = {
-    { 2,  4,  6,  2,  4},
-    { 1,  2, -1,  3,  8},
-    {-3,  1, -2,  1, -2},
-    { 1,  3, -3, -2,  4}
-  };
+  double E[numRows][numCols] = {
+    {2, 4, 6, 2, 4},
+    {1, 2, -1, 3, 8},
+    {-3, 1, -2, 1, -2},
+    {1, 3, -3, -2, 6}
+  }; // solução exata: 2, 1, -1, 1
 
-  
+  print_matrix(E);
+  gauss(E);
 }
--- aulas/aula-6/jacobi.c
+++ aulas/aula-6/jacobi.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 
-#define l 4
-#define c 5
+#define r 3
 
-void jacobi(double E[l][c], double kick[l], int it) {
-  for (int i = 0; i < it; i++) {
-    double resp[l] = {};
-    for (int j = 0; j < l; j++) {
-      double bj = E[j][c - 1];
-      double soma = 0;
-      for (int k = 0; k < c; k++) {
-        if (k != j) {
-          E[j][k] * kick[k];
-        }
+void jacobi(double A[r][r], double B[r], double est[r], int n) {
+  double next[r];
+
+  for (int k = 0; k < n; k++) {
+    // Para cada iteração
+    for (int i = 0; i < r; i++) {
+      // Para cada linha
+      double bi = B[i];
+      for (int j = 0; j < r; j++) {
+        // Para cada coluna
+        if (j != i)
+          bi -= A[i][j] * est[j];
       }
-      double xj = (bj + soma) / E[j][j];
-      temp[j] = xj;
-      printf("")
+      bi /= A[i][i];
+
+      printf("x%d^(%d) = %.16lf\t", i + 1, k + 1, bi);
+
+      next[i] = bi;
     }
+    printf("\n");
+
+    for (int i = 0; i < r; i++)
+      est[i] = next[i];
+    // atualizar a estimativa
   }
 }
 
 int main() {
-  double E[l][c] = {
-    {4, 2, -1, 11},
-    {1, -5, 1, -4},
-    {1, 1, -6, 9}
-  };
+  double A[r][r] = {
+    {4, 1, -1},
+    {-1, 3, 1},
+    {1, -1, 5}
+  }; // Matriz dos coeficientes
 
-  double kick[c] = {0, 0, 0};
+  double B[r] = {5, 6, 4};
+  // Matriz dos termos independentes
+
+  double est[r] = {0, 0, 0};
+  int n = 10;
+
+  jacobi(A, B, est, n);
 }
