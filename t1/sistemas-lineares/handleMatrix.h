f1bd7f2aa718eacfd61dc7c081c4b2babb201efb
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2022-09-22 22:27:06 -0300

    Deleted old auxiliar methods

--- t1/sistemas-lineares/handleMatrix.h
+++
@@ -1,8 +0,0 @@
-#ifndef HANDLE_MATRIX_H
-#define HANDLE_MATRIX_H
-
-void printM(int nRows, int nCols, double m[nRows][nCols]);
-
-void printMFormatted(int nRows, int nCols, double m[nRows][nCols]);
-
-#endif
--- t1/sistemas-lineares/operations.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPERATIONS_H
-#define OPERATIONS_H
-
-void timesK(int nRows, int nCols, double m[nRows][nCols], int l, double k);
-
-void swap(int nRows, int nCols, double m[nRows][nCols], int l1, int l2);
-
-#endif
